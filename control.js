a89a57b0212a5db8c7700086ccb9f192d84a80b9
Selmi Med Dhia <monjihidousi@gmail.com>
2025-01-25 20:25:01 +0100

    sub-chapters work now

--- control.js
+++ control.js
@@ -6,6 +6,11 @@ function subject(name){
 function chapter(title){
     this.title = title;
     this.crossed = false;
+    this.subChapters = [];
+}
+function subChapter(title){
+    this.title = title;
+    this.crossed = false;
 }
 
 function saveValue(){
@@ -28,6 +33,13 @@ function setup(){
                     htmlCode += "crossed";
                 }
                 htmlCode += " ' onclick='showOptions2(event,"+sub+","+chap+")'>" +subjects[sub].chapters[chap].title + "</div>"
+                for(subchap in subjects[sub].chapters[chap].subChapters){
+                    htmlCode += "<div class='alert alert-dark sub-chapter-title ";
+                    if(subjects[sub].chapters[chap].subChapters[subchap].crossed){
+                        htmlCode += "crossed";
+                    }
+                    htmlCode +=" ' onclick='showOptions3(event,"+sub+","+chap+","+subchap+")' >"+subjects[sub].chapters[chap].subChapters[subchap].title + "</div>";
+                }
             }
             htmlCode += "<button class='plusButton' onclick='showContainer2("+sub+")'>-</button>"
             htmlCode += "</div>"
@@ -37,13 +49,30 @@ function setup(){
 }
 var concernedSubjectGlobal;
 var concernedChapterGlobal;
+var concernedSubChapterGlobal;
 function showOptions1(event, concernedSubject){
-    concernedSubjectGlobal = concernedSubject;
-    concernedChapterGlobal = 0;
-    let elem = document.getElementById('options-container1');
-    elem.style.left = (event.clientX-1)+"px";
-    elem.style.top = (event.clientY-1)+"px";
-    elem.style.visibility = 'visible';
+    if (free){
+        concernedSubjectGlobal = concernedSubject;
+        concernedChapterGlobal = 0;
+        let elem = document.getElementById('options-container1');
+        elem.style.left = (event.clientX-1)+"px";
+        elem.style.top = (event.clientY-1)+"px";
+        elem.style.visibility = 'visible';
+        free = false;
+    }
+}
+
+function showOptions3(event, concernedSubject, concernedChapter, concernedSubChapter){
+    if (free){
+        concernedSubjectGlobal = concernedSubject;
+        concernedChapterGlobal = concernedChapter;
+        concernedSubChapterGlobal = concernedSubChapter;
+        let elem = document.getElementById('options-container3');
+        elem.style.left = (event.clientX-1)+"px";
+        elem.style.top = (event.clientY-1)+"px";
+        elem.style.visibility = 'visible';
+        free = false;
+    }
 }
 
 function deleteSubject(){
@@ -68,7 +97,7 @@ function showContainer3(){
     document.getElementById('confirm-button2').value = -1;
 }
 function addChapterIndex(index){
-    let subjects = localStorage.getItem('subjects')
+    let subjects = localStorage.getItem('subjects');
     subjects = JSON.parse(subjects);
     subjects[concernedSubjectGlobal].chapters.splice(index, 0, new chapter( document.getElementById('floating-input2').value ));
     subjects = JSON.stringify(subjects);
@@ -78,7 +107,7 @@ function addChapterIndex(index){
     hideContainer2();
 }
 function cross(){
-    let subjects = localStorage.getItem('subjects')
+    let subjects = localStorage.getItem('subjects');
     subjects = JSON.parse(subjects);
     subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].crossed = true;
     subjects = JSON.stringify(subjects);
@@ -86,13 +115,31 @@ function cross(){
     hideOptionsContainer2();
     setup();
 }
+function crossSub(){
+    let subjects = localStorage.getItem('subjects');
+    subjects = JSON.parse(subjects);
+    subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].subChapters[concernedSubChapterGlobal].crossed = true;
+    subjects = JSON.stringify(subjects);
+    localStorage.setItem('subjects', subjects);
+    hideOptionsContainer3();
+    setup();
+}
 function uncross(){
-    let subjects = localStorage.getItem('subjects')
+    let subjects = localStorage.getItem('subjects');
     subjects = JSON.parse(subjects);
     subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].crossed = false;
     subjects = JSON.stringify(subjects);
     localStorage.setItem('subjects', subjects);
     hideOptionsContainer2();
+    setup();
+}
+function uncrossSub(){
+    let subjects = localStorage.getItem('subjects');
+    subjects = JSON.parse(subjects);
+    subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].subChapters[concernedSubChapterGlobal].crossed = false;
+    subjects = JSON.stringify(subjects);
+    localStorage.setItem('subjects', subjects);
+    hideOptionsContainer3();
     setup();
 }
 function deleteChapter(){
@@ -111,13 +158,33 @@ function deleteChapter(){
     hideOptionsContainer2();
     setup();
 }
+function deleteSubChapter(){
+    let subjects = localStorage.getItem('subjects');
+    subjects = JSON.parse(subjects);
+
+    let tmp = [];
+    for(let subchap=0; subchap < subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].subChapters.length; subchap++){
+        if (subchap != concernedSubChapterGlobal){
+            tmp.push(subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].subChapters[subchap]);
+        }
+    }
+    subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].subChapters = tmp;
+    subjects = JSON.stringify(subjects);
+    localStorage.setItem('subjects', subjects);
+    hideOptionsContainer3();
+    setup();
+}
+
 function showOptions2(event, concernedSubject, concernedChapter){
-    concernedSubjectGlobal = concernedSubject;
-    concernedChapterGlobal = concernedChapter;
-    let elem = document.getElementById('options-container2');
-    elem.style.left = (event.clientX-1)+"px";
-    elem.style.top = (event.clientY-1)+"px";
-    elem.style.visibility = 'visible';
+    if (free){
+        concernedSubjectGlobal = concernedSubject;
+        concernedChapterGlobal = concernedChapter;
+        let elem = document.getElementById('options-container2');
+        elem.style.left = (event.clientX-1)+"px";
+        elem.style.top = (event.clientY-1)+"px";
+        elem.style.visibility = 'visible';
+        free = false;
+    }
 }
 
 function showContainer4(){
@@ -126,17 +193,42 @@ function showContainer4(){
     document.getElementById('confirm-button2').onclick = () => addChapterIndex(concernedChapterGlobal+1);
     document.getElementById('confirm-button2').value = -1;
 }
+
+function showContainer5(){
+    hideOptionsContainer2();
+    showContainer3(concernedSubjectGlobal, concernedChapterGlobal);
+}
 function hideOptionsContainer1(){
     document.getElementById('options-container1').style.visibility = 'hidden';
+    free = true;
 }
 function hideOptionsContainer2(){
     document.getElementById('options-container2').style.visibility = 'hidden';
+    free = true;
+}
+function hideOptionsContainer3(){
+    document.getElementById('options-container3').style.visibility = 'hidden';
+    free = true;
 }
 var free;
 function showContainer1(){
     if (free){
         document.getElementById('adding-subject-container').style = "visibility:visible";
         document.getElementById('floating-input1').focus();
+        free = false;
+    }
+}
+function showContainer3(concernedSubject, concernedChapter){
+    if (free){
+        document.getElementById('adding-sub-chapter-container').style = "visibility:visible";
+        document.getElementById('floating-input3').focus();
+
+        let subjects = localStorage.getItem('subjects');
+        subjects = JSON.parse(subjects);
+        document.getElementById("adding-container-header2").innerHTML = "Adding a sub-chapter to " + subjects[concernedSubject].chapters[concernedChapter].title;
+        document.getElementById('confirm-button3').onclick = () => addSubChapter();
+        document.getElementById('floating-input3').focus();
+
         free = false;
     }
 }
@@ -154,6 +246,10 @@ function showContainer2(concernedSubject){
 }
 function hideContainer1(){
     document.getElementById('adding-subject-container').style = "visibility:hidden";
+    free = true;
+}
+function hideContainer3(){
+    document.getElementById('adding-sub-chapter-container').style = "visibility:hidden";
     free = true;
 }
 function hideContainer2(){
@@ -176,6 +272,15 @@ function attemptEnabling2(){
     }
     else{
         document.getElementById('confirm-button2').disabled = false;
+    }
+}
+function attemptEnabling3(){
+    const val = document.getElementById('floating-input3').value;
+    if (val == ""){
+        document.getElementById('confirm-button3').disabled = true;
+    }
+    else{
+        document.getElementById('confirm-button3').disabled = false;
     }
 }
 function addSubject(){
@@ -204,6 +309,17 @@ function addChapter(concernedSubject){
     hideContainer2();
 }
 
+function addSubChapter(){
+    let subjects = localStorage.getItem('subjects')
+    subjects = JSON.parse(subjects);
+    subjects[concernedSubjectGlobal].chapters[concernedChapterGlobal].subChapters.push(new subChapter( document.getElementById('floating-input3').value ));
+    subjects = JSON.stringify(subjects);
+    localStorage.setItem('subjects', subjects);
+    document.getElementById('floating-input3').value = "";
+    setup();
+    hideContainer3();
+}
+
 function checkEnter2(event){
     if(event.key == "Enter" && document.getElementById('confirm-button2').disabled == false){
         if (document.getElementById('confirm-button2').value != -1){
@@ -211,6 +327,12 @@ function checkEnter2(event){
         }else{
             addChapterIndex(concernedChapterGlobal+1);
         }
+    }
+}
+function checkEnter3(event){
+    if(event.key == "Enter" && document.getElementById('confirm-button3').disabled == false){
+        addSubChapter();
+        
     }
 }
 function checkEnter1(event){
